7a6b8e1808951ca6d0e60fe8028f0380f1e609eb
vibhuti-jadav <vibhutij109@gmail.com>
2025-11-03 16:59:43 +0530

    socket

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
-
 import express from "express";
-import {fileURLToPath} from "url"
-import { dirname , join } from "path";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 import { createServer } from "http";
 import { Server } from "socket.io";
 
-const app = express()
+const app = express();
 
 const __fileName = fileURLToPath(import.meta.url);
 const __dirName = dirname(__fileName);
-const publicDirectory = join (__dirName,"./public")
+const publicDirectory = join(__dirName, "./public");
 
-app.use(express.static(publicDirectory))
+app.use(express.static(publicDirectory));
 
 const server = createServer(app);
 
 const io = new Server(server);
 
-io.on("connection",(socket)=>{
-    console.log("new websocket connection established")
+io.on("connection", (socket) => {
+  console.log("new websocket connection established");
 
-    // io.emit("message","welcome")
+  // io.emit("message","welcome")
 
-    socket.emit("message","welcome")
-})
+  socket.emit("message", "welcome");
+});
 
-const port = 5000
+const port = 5000;
 
-server.listen(port,()=>{
-    console.log(`server running on port`,port)
-})
-
-
+server.listen(port, () => {
+  console.log(`server running on port`, port);
+});
