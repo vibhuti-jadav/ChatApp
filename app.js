55f11da08cff0f8cbbfef288d697a1c5037129df
vibhuti-jadav <vibhutij109@gmail.com>
2025-11-03 16:22:57 +0530

    loading page

---
+++ app.js
@@ -0,0 +1,34 @@
+
+import express from "express";
+import {fileURLToPath} from "url"
+import { dirname , join } from "path";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express()
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirName = dirname(__fileName);
+const publicDirectory = join (__dirName,"./public")
+
+app.use(express.static(publicDirectory))
+
+const server = createServer(app);
+
+const io = new Server(server);
+
+io.on("connection",(socket)=>{
+    console.log("new websocket connection established")
+
+    // io.emit("message","welcome")
+
+    socket.emit("message","welcome")
+})
+
+const port = 5000
+
+server.listen(port,()=>{
+    console.log(`server running on port`,port)
+})
+
+
