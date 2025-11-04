c320e0f664adc81804e070334d7aed0e4a5d963d
vibhuti-jadav <vibhutij109@gmail.com>
2025-11-04 15:41:04 +0530

    c to s and location

--- app.js
+++ app.js
@@ -19,9 +19,20 @@ const io = new Server(server);
 io.on("connection", (socket) => {
   console.log("new websocket connection established");
 
+  io.emit("newConnection","a new user joined")
+
   // io.emit("message","welcome")
 
   socket.emit("message", "welcome");
+
+socket.on("sendMessage",(msg)=>{
+  console.log(msg)
+})
+
+socket.on("location",(lat,lon)=>{
+  socket.emit("message", `https://google.com/maps?q=${lat},${lon}`);
+});
+
 });
 
 const port = 5000;
--- public/chat.js
+++ public/chat.js
@@ -3,3 +3,35 @@ const socket = io()
 socket.on("message",(msg)=>{
     console.log(msg)
 })
+
+socket.on("newConnection",(msg)=>{
+    console.log(msg)
+})
+
+
+document.querySelector("#msg-form").addEventListener("submit",(e)=>{
+    e.preventDefault();
+
+    let message = e.target.elements.message.value;
+
+    console.log(message)
+
+    socket.emit("sendMessage",message)
+
+    e.target.elements.message.value = "";
+})
+
+
+document.querySelector("#location").addEventListener("click",()=>{
+    if(!navigator.geolocation){
+        return alert("your device is not supported to geo location");
+    }
+
+    navigator.geolocation.getCurrentPosition((position)=>{
+        const lat = position.coords.latitude;
+
+        const lon = position.coords.longitude;
+
+        socket.emit("location",lat,lon)
+    })
+})
