e11d14e3ad282f5cf8ac36cfb0831713335dedaa
vibhuti-jadav <vibhutij109@gmail.com>
2025-11-07 16:42:46 +0530

    enable-disable

--- public/chat.js
+++ public/chat.js
@@ -4,33 +4,38 @@ socket.on("message",(msg)=>{
     console.log(msg)
 })
 
+
+// const messageTemplate = document.querySelector("#msg-template").innerHTML;
+
+// const message = document.querySelector("#message")
+
 socket.on("newConnection",(msg)=>{
     console.log(msg)
 })
+
+
 
 const $messageForm = document.querySelector("#msg-form");
 
 const $messageInput = $messageForm.querySelector("input");
 
-
 const $messageButton = $messageForm.querySelector("button")
-
 
 $messageForm.addEventListener("submit",(e)=>{
     e.preventDefault();
+   
     $messageButton.setAttribute("disabled","disabled")
 
-    let message = e.target.element.message.value;
+    let message = e.target.elements.message.value;
 
-    socket.emit("sendMessage",message,(act,error)=>{
-        if(error){
-            return console.log(error.message)
-        }
-    })
+    socket.emit("sendMessage",message)
+
+    $messageButton.removeAttribute("disabled");
+
+    e.target.elements.message.value = ""
+
+    $messageInput.focus()
 })
-
-
-
 
 // document.querySelector("#msg-form").addEventListener("submit",(e)=>{
 //     e.preventDefault();
@@ -44,15 +49,35 @@ $messageForm.addEventListener("submit",(e)=>{
 //     e.target.elements.message.value = "";
 // })
 
+const $locationButton = document.querySelector("#location");
 
-document.querySelector("#location").addEventListener("click",()=>{
+$locationButton.addEventListener("click",()=>{
+    $locationButton.setAttribute("disabled","disabled");
+
     if(!navigator.geolocation){
-        return alert("your device is not supported to geo location");
+        return alert("your browser does not supported geo location")
     }
 
     navigator.geolocation.getCurrentPosition((position)=>{
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
+        const lat = position.coords.latitude
+
+        const lon = position.coords.longitude
+
         socket.emit("location",lat,lon)
+
+      $locationButton.removeAttribute("disabled")
     })
+
 })
+
+// document.querySelector("#location").addEventListener("click",()=>{
+//     if(!navigator.geolocation){
+//         return alert("your device is not supported to geo location");
+//     }
+
+//     navigator.geolocation.getCurrentPosition((position)=>{
+//         const lat = position.coords.latitude;
+//         const lon = position.coords.longitude;
+//         socket.emit("location",lat,lon)
+//     })
+// })
